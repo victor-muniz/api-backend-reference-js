0c1cae06c9f52340eb0fa23351f656d139425440
victor-muniz <19287031+victor-muniz@users.noreply.github.com>
2020-05-19 17:02:49 -0300

    melhorando teste unitário

--- test/unit/validation/data-validator-test.js
+++ test/unit/validation/data-validator-test.js
@@ -1,35 +1,60 @@
 const { expect } = require('chai');
 const Joi = require('joi');
 
+const DataValidationError = require('../../../lib/error/DataValidationError')
 const DataValidator = require('../../../lib/validation/data-validator')
 
-const invalidName = { name: '1233' };
+const invalidPerson = { name: '1233' };
 
-const validName = { name: 'Victor' }
+const validPerson = { name: 'Victor', age: 10 }
 
-const rule = { name: Joi.string().regex(/^[A-Z]+$/).uppercase() };
+const rule = {
+    name: Joi.string().regex(/^[A-Z]+$/).uppercase().required(),
+    age: Joi.number().integer().min(0).required()
+};
 
-describe("validate(data, validationRule, validationOptions)", function () {
+describe("DataValidator.validate(data, validationRule, validationOptions)", function () {
     this.timeout(Infinity);
 
     context('when data and validationRule are not present', function () {
-        it("should return exception", async () => {
-            expect(() => new DataValidator().validate()).to.throw('Validation data is required!');
+        it("should return an exception", async () => {
+            expect(() => new DataValidator().validate()).to.throw(DataValidationError, 'Validation data is required!');
         });
     });
 
     context('when data is present and validationRule is not present', function () {
-        it("should return exception", async () => {
-            expect(() => new DataValidator().validate(validName)).to.throw('Validation rule is required!');
+        it("should return an exception", async () => {
+            expect(() => new DataValidator().validate(validPerson)).to.throw(DataValidationError, 'Validation rule is required!');
         });
 
     });
 
     context('when valid data and valid validationRule are present', function () {
         it("should return object validated", async () => {
-            const validatedData = new DataValidator().validate(validName,rule)
-            expect(validatedData).to.eql({ name: 'VICTOR' });
+            const validatedData = new DataValidator().validate(validPerson, rule)
+            expect(validatedData).to.eql({ name: 'VICTOR', age: 10 });
         });
 
+    });
+
+    context('when invalid data and valid validationRule are present', function () {
+        it("should return an exception", async () => {
+
+            expect(() => new DataValidator().validate(invalidPerson, rule)).to.throw(DataValidationError, '[{"message":"\\"name\\" with value \\"1233\\" fails to match the required pattern: /^[A-Z]+$/","path":["name"],"type":"string.regex.base","context":{"pattern":{},"value":"1233","key":"name","label":"name"}}]');
+
+        });
+    });
+
+
+    context('when invalid data, valid validationRule and custom validationOptions(abortEarly=false) are present', function () {
+        it("should return an exception", async () => {
+            const customValidationOptions = {
+                abortEarly: false,
+                allowUnknown: false,
+                stripUnknown: false
+            };
+            expect(() => new DataValidator().validate(invalidPerson, rule, customValidationOptions)).to.throw(DataValidationError, '[{"message":"\\"name\\" with value \\"1233\\" fails to match the required pattern: /^[A-Z]+$/","path":["name"],"type":"string.regex.base","context":{"pattern":{},"value":"1233","key":"name","label":"name"}},{"message":"\\"age\\" is required","path":["age"],"type":"any.required","context":{"key":"age","label":"age"}}]');
+
+        });
     });
 });
